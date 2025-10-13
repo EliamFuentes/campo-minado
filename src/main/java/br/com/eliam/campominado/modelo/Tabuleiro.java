ddba204101d010eb0d04aa11a03841c69b31a256
eliamfuentes <eliamfuentes123@gmail.com>
2025-10-13 14:50:15 -0300

    Adicionando métodos no Campo e testes

--- src/main/java/br/com/eliam/campominado/modelo/Tabuleiro.java
+++ src/main/java/br/com/eliam/campominado/modelo/Tabuleiro.java
@@ -1,4 +1,6 @@
 package br.com.eliam.campominado.modelo;
 
 public class Tabuleiro {
+
+
 }
--- src/test/java/br/com/eliam/campominado/modelo/CampoTeste.java
+++ src/test/java/br/com/eliam/campominado/modelo/CampoTeste.java
@@ -1,11 +1,10 @@
 package br.com.eliam.campominado.modelo;
 
+import br.com.eliam.campominado.excesao.ExplosaoException;
 import org.junit.Before;
 import org.junit.Test;
 
-
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 public class CampoTeste {
 
@@ -60,5 +59,47 @@ public class CampoTeste {
         campo.alternarMarcacao();
         campo.alternarMarcacao();
         assertFalse(campo.isMarcado());
+    }
+
+    @Test
+    public void testeAbrirNaoMinadoNaoMarcado() {
+        assertTrue(campo.abrir());
+    }
+
+    @Test
+    public void testeAbrirNaoMinadoMarcado() {
+        campo.alternarMarcacao();
+        assertFalse(campo.abrir());
+    }
+
+    @Test
+    public void testeAbrirMinadoMarcado() {
+        campo.alternarMarcacao();
+        campo.minar();
+        assertFalse(campo.abrir());
+    }
+
+    @Test
+    public void testeAbrirMinadoNaoMarcado() {
+        campo.minar();
+
+        assertThrows(ExplosaoException.class, () -> {
+            campo.abrir();
+        });
+    }
+
+    @Test
+    public void testeAbrirComVizinhos() {
+
+        Campo vizinhoDoVizinho1 = new Campo (1, 1);
+
+        Campo vizinho1 = new Campo (2, 2);
+        vizinho1.adicionarVizinho(vizinhoDoVizinho1);
+
+        campo.adicionarVizinho(vizinho1);
+
+        campo.abrir();
+
+        assertTrue(vizinho1.isAberto() && vizinhoDoVizinho1.isAberto());
     }
 }
